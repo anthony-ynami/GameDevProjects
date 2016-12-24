f237c3198316f347ae01101c9f3e4ae58e4eb6b8
anthony-ynami <anthony.ynami@nyu.edu>
2016-12-23 21:18:13 -0500

    rube goldberg

---
+++ pitagora suittchi/Assets/scripts/CamControl.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamControl : MonoBehaviour
+{
+
+
+    public GameObject[] Cameras;
+    private int currentCam;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        DeactivateCameras();
+        Cameras[0].SetActive(true);
+        currentCam = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            DeactivateCameras();
+
+            if (currentCam < Cameras.Length - 1)
+            {
+                currentCam += 1;
+            }
+            else
+            {
+                currentCam = 0;
+            }
+
+
+            Cameras[currentCam].SetActive(true);
+
+        }
+
+    }
+
+    public void activateCamera(int _camNumber)
+    {
+
+        Cameras[_camNumber - 1].SetActive(true);
+        currentCam = _camNumber - 1;
+    }
+
+    public void DeactivateCameras()
+    {
+        for (int i = 0; i < Cameras.Length; i++)
+        {
+            Cameras[i].SetActive(false);
+        }
+
+    }
+}
---
+++ pitagora suittchi/Assets/scripts/CamTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamTrigger : MonoBehaviour {
+
+	//public GameObject camToActivate;
+	public int camNumber;
+
+	public CamControl controller;
+
+	void OnTriggerEnter(Collider other){
+
+		controller.DeactivateCameras();
+		controller.activateCamera(camNumber);
+
+	}
+}
